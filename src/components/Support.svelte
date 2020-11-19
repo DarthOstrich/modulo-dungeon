5eccddf677a0b5cdfb553c71cfa32a43e251ccb4
Jared <jarednutt@outlook.com>
2020-11-18 17:36:37 -0800

    fixing kofi url

--- src/components/Support.svelte
+++ src/components/Support.svelte
@@ -26,5 +26,5 @@
   <h2>Support this Project</h2>
   <p>As you may know, streaming music pays pennies. If you enjoy this project, any additional pennies will ensure the lights stay on after the music is done spinning.</p>
   <p>There’s a number of ways you can support this project. You can <a href="https://modulo.fanlink.to/traveler">stream or buy it on any of the major platforms</a>.</p>
-  <p>You can also <a href="buy me a cup of coffee.">buy me a pint of mead.</a></p>
+  <p>You can also <a href="https://ko-fi.com/modulo">buy me a pint of mead.</a></p>
 </section>
