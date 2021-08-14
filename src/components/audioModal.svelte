ed858001efe254b683812412bdb9f4d9a99b6efa
Jared <jared@digitalnutt.com>
2021-08-14 14:39:43 -0700

    removing old files

--- src/components/audioModal.svelte
+++
@@ -1,8 +0,0 @@
-<p>Note: If you don’t have either of those services, the music is also available here:</p>
-<ul>
-  <li><a href="https://music.amazon.com/artists/B0032DSEJQ/modulo">Amazon Music</a></li>
-  <li><a href="https://modulous.bandcamp.com/">Bandcamp</a></li>
-  <li><a href="https://www.deezer.com/us/artist/1289645">Deezer</a></li>
-  <li><a href="https://us.napster.com/artist/modulo">Napster</a></li>
-</ul>
-<p></p>
